3ad067f44aed365be06c0f932ac03b60b0fc98be
Maia Manze <142365480+maiamanze@users.noreply.github.com>
2025-11-28 13:57:41 -0300

    World actualizado y en TypeScript

---
+++ tests/support/world.ts
@@ -0,0 +1,48 @@
+import "dotenv/config";
+import {
+    setWorldConstructor,
+    setDefaultTimeout,
+    BeforeAll,
+    AfterAll,
+    Before,
+    After,
+} from'@cucumber/cucumber';
+
+import { Browser, BrowserContext, chromium, Page } from "playwright";
+import { DatosMeteorologicosPage } from "../../pages/DatosMeteorologicosPage";
+
+setDefaultTimeout(60 * 1000);
+
+let browser: Browser;
+
+export class CustomWorld {
+    browser!: Browser;
+    context!: BrowserContext;
+    page!: Page;
+    datosPage!: DatosMeteorologicosPage;
+
+    env = process.env;
+}
+
+setWorldConstructor(CustomWorld);
+
+BeforeAll(async function () {
+    browser = await chromium.launch({ headless: true });
+});
+
+Before(async function (this: CustomWorld) {
+    this.browser = browser;
+    this.context = await browser.newContext();
+    this.page = await this.context.newPage();
+    this.datosPage = new DatosMeteorologicosPage(this.page);
+});
+
+After(async function (this: CustomWorld) {
+    await this.page?.close();
+    await this.context?.close();
+});
+
+AfterAll(async function () {
+    await browser?.close();
+});
+
